068f02ccdec6cf08617bdc8a5021fcb2da24616c
Jesse Visser <hallo@jnvisser.nl>
2022-08-04 09:58:01 +0200

    Make config settings configurable via environment variables

--- config/moneybird.php
+++ config/moneybird.php
@@ -2,16 +2,16 @@
 
 return [
 
-    'redirect_uri' => 'urn:ietf:wg:oauth:2.0:oob',
+    'redirect_uri' => env('MONEYBIRD_REDIRECT_URI', 'urn:ietf:wg:oauth:2.0:oob'),
 
-    'client_id' => '',
+    'client_id' => env('MONEYBIRD_CLIENT_ID'),
 
-    'client_secret' => '',
+    'client_secret' => env('MONEYBIRD_CLIENT_SECRET'),
 
-    'authorization_token' => '',
+    'authorization_code' => env('MONEYBIRD_AUTHORIZATION_CODE'),
 
-    'access_token' => '',
+    'access_token' => env('MONEYBIRD_ACCESS_TOKEN'),
 
-    'administration_id' => '',
+    'administration_id' => env('MONEYBIRD_ADMINISTRATION_ID'),
 
 ];
